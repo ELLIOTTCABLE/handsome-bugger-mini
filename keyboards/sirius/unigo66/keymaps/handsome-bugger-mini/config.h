c240edb8288fb56b3f571292932c379d8b288a20
ELLIOTTCABLE <git@ell.io>
2020-01-16 23:19:45 -0700

    (fix) Tweak tap-hold keys to perform better on the split-wireless pair

--- keyboards/sirius/unigo66/keymaps/handsome-bugger-mini/config.h
+++ keyboards/sirius/unigo66/keymaps/handsome-bugger-mini/config.h
@@ -1,3 +1,11 @@
 #pragma once
 
-// place overrides here
+// disable old style macro handling: MACRO() & action_get_macro
+#define NO_ACTION_MACRO
+
+// disable calling of action_function() from the fn_actions array (deprecated)
+#define NO_ACTION_FUNCTION
+
+// trying to improve _T behaviour on the UniGo66 with these two enabled together:
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
