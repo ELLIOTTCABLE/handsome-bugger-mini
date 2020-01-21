0cd7e13acda9b4f31e4bc7c95c7676836364f5b6
ELLIOTTCABLE <git@ell.io>
2020-01-20 23:06:22 -0700

    (- re) Enable link-time optimization

--- keyboards/sirius/unigo66/keymaps/handsome-bugger-mini/config.h
+++ keyboards/sirius/unigo66/keymaps/handsome-bugger-mini/config.h
@@ -2,12 +2,6 @@
 
 #define TAPPING_TERM 175
 
-// disable old style macro handling: MACRO() & action_get_macro
-#define NO_ACTION_MACRO
-
-// disable calling of action_function() from the fn_actions array (deprecated)
-#define NO_ACTION_FUNCTION
-
 // trying to improve _T behaviour on the UniGo66 with these two enabled together:
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
